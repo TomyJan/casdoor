3ba89a72a720d25b95bcb40dcd9e8779a70de498
TomyJan <TomyJan6@gmail.com>
2026-09-25 16:24:04 +0800

    Fix formatting in authz.go for logout route

--- authz/authz.go
+++ authz/authz.go
@@ -106,7 +106,7 @@ var defaultApiRules = [][]string{
 	{"*", "*", "POST", "/api/acs", "*", "*"},
 	{"*", "*", "GET", "/api/saml/metadata", "*", "*"},
 	{"*", "*", "*", "/api/saml/redirect", "*", "*"},
-  {"*", "*", "*", "/api/saml/logout", "*", "*"},
+	{"*", "*", "*", "/api/saml/logout", "*", "*"},
 	{"*", "*", "*", "/cas", "*", "*"},
 	{"*", "*", "*", "/scim", "*", "*"},
 	{"*", "*", "*", "/api/webauthn", "*", "*"},
